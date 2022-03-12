a3732a2294529b341f7b1690728943a4ba083b2f
Samiat20 <samiat.olanrewaju20@gmail.com>
2022-03-12 12:54:13 +0100

    updating the project section

---
+++ app.js
@@ -0,0 +1,12 @@
+const projects= [
+    {
+    name: "todo",
+    description:"This is a todo app where you can register your daily, weekly or general tasks ",
+    link: '#',
+    img:'./img/projects/todo.png'
+},
+    {
+        name: "the gang",
+        description:"This is a fan page for cartoon characters"
+    }
+]
