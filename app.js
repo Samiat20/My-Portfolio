0692263f9de925f56a2a397e50d01d748d61f730
Samiat20 <samiat.olanrewaju20@gmail.com>
2022-03-12 13:48:36 +0100

    populating the project from Javascript

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const projects= [
     {
-    name: "todo",
+    name: "Todo",
     description:"This is a todo app where you can register your daily, weekly or general tasks ",
-    link: '#',
+    link:"https://wonderful-goodall-dede27.netlify.app/",
     img:'./img/projects/todo.png'
-},
+    },
     {
-        name: "the gang",
-        description:"This is a fan page for cartoon characters"
+        name: "The gang",
+        description:"This is a fan page for cartoon characters",
+        link: "https://condescending-meninsky-b3526e.netlify.app/",
+        img:"./img/projects/shapes.png" 
     }
 ]
+
+//populate the projects section
+// grab the section
+var project= document.querySelector(".projects")
+console.log('x')
+projects.forEach(x=>{
+
+  var div =  document.createElement("div")
+  div.className="project"
+  div.innerHTML=`<a href="${x.link}" target="_blank"> <img src="${x.img}" alt="The Gang"></a>
+  <h2> ${x.name} </h2>
+  <p> Description: "${x.description}"</p>`
+  project.appendChild(div)
+})
+//create a loop to create the a,img,p,h2 element
+// create a function to populate the section with the element created in the loop
+// append the elements as a child of the section
